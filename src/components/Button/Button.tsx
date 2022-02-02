f1a3c370fd420508780fb554b7e68224293fed08
Lucas Couto <lucas@orbithub.com.br>
2022-02-02 09:50:02 -0300

    chore: add css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './Button.module.css'
 
 export interface ButtonProps {
   label: string
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>
+  return <button className={styles.base}>{props.label}</button>
 }
 
 export default Button
