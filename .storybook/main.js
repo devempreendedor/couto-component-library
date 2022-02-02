282e287c8e649a63ce55eb13c610472e03c6c16c
Lucas Couto <lucas@orbithub.com.br>
2022-02-02 10:34:18 -0300

    chore: add scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,8 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
   ],
   "framework": "@storybook/react",
   "core": {
